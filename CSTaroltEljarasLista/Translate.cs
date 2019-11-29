deaf85db58452cdc1704b4437e95cfb43a104f77
d7p4n4 <bercikezs199999999@gmail.com>
2019-11-29 20:55:56 +0100

    1.20191129.3

---
+++ CSTaroltEljarasLista/Translate.cs
@@ -0,0 +1,46 @@
+﻿using CSAc4yGetXmls;
+using CSAc4yObjectGetGuidCAP;
+using d7p4n4Namespace.Final.Class;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CSTaroltEljarasLista
+{
+    public class Translate
+    {
+        public string XmlToHtmlTransform(SqlConnection connection)
+        {
+            Translate translate = new Translate();
+            Ac4yGetXml ac4YGetXml = new Ac4yGetXml(connection);
+            Ac4yGetGuid ac4YGetGuid = new Ac4yGetGuid(connection);
+            Deserializer deserializer = new Deserializer();
+            Serializer serializer = new Serializer();
+            string finalXml = "";
+
+            List<string> list = ac4YGetGuid.GetGuidByTemplateGuid();
+            List<string> xmlList = ac4YGetXml.GetXmlsByGuids(list);
+            List<TaroltEljaras> taroltEljarasok = new List<TaroltEljaras>();
+
+            foreach (var l in list)
+            {
+                Console.WriteLine(l);
+            }
+            foreach (var xml in xmlList)
+            {
+                //Console.WriteLine(xml);
+                taroltEljarasok.Add(deserializer.deser(xml));
+            }
+
+            Console.WriteLine(taroltEljarasok.Count);
+
+            TaroltEljarasLista taroltEljarasLista = new TaroltEljarasLista() { taroltEljarasLista = taroltEljarasok };
+
+            finalXml = serializer.serialize(taroltEljarasLista);
+            return finalXml;
+        }
+    }
+}
