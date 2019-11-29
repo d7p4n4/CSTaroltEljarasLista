b3fcdb2054f52e15e0ac70b72d549184fed4e438
d7p4n4 <bercikezs199999999@gmail.com>
2019-11-29 14:15:16 +0100

    Add project files.

---
+++ CSTaroltEljarasLista/Serializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CSTaroltEljarasLista
+{
+    public class Serializer
+    {
+        public string serialize(TaroltEljarasLista taroltEljarasLista)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(TaroltEljarasLista));
+            var xml = "";
+
+            using (var writer = new StringWriter())
+            {
+                using (XmlWriter xmlWriter = XmlWriter.Create(writer))
+                {
+                    serializer.Serialize(writer, taroltEljarasLista);
+                    xml = writer.ToString(); // Your XML
+                }
+            }
+
+            return xml;
+        }
+    }
+}
---
+++ CSTaroltEljarasLista/TaroltEljarasLista.cs
@@ -0,0 +1,12 @@
+﻿using d7p4n4Namespace.Final.Class;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSTaroltEljarasLista
+{
+    public class TaroltEljarasLista
+    {
+        public List<TaroltEljaras> taroltEljarasLista { get; set; }
+    }
+}
